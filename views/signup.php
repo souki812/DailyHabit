7ee024691936b49fad4a7572b0733e17ede56523
souki812 <alamisouki@gmail.com>
2016-04-10 16:00:03 -0400

    Added Navbar Sign-up page

--- views/signup.php
+++ views/signup.php
@@ -8,6 +8,40 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     </head>
     <body>
+        
+        <div class="navbar navbar-default">
+            
+            <div class="container">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand">DailyHabit</a>
+                </div>
+                
+                <div class="collapse navbar-collapse"></div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#home">Home</a></li> <!--active makes it default page-->
+                     <li><a href="#about">About</a></li>
+                      <li><a href="#=testimonial">Testimonial</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         <div class="container">
             <div class="row">
                 <div class="col-xs-3">
