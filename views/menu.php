f498324c113c3e45d76d3dfe0b0cdc82dbef4753
souki812 <alamisouki@gmail.com>
2016-04-12 16:25:22 -0400

    Created navigation bar for user home page

--- views/menu.php
+++ views/menu.php
@@ -1,9 +1,27 @@
-                <div class="col-xs-3">
-                    <nav>
-                        <ul class="nav nav-pills nav-stacked">
-                            <li <?php if ($_SERVER['REQUEST_URI'] == '/') echo 'class="active"'; ?>><a href="../">Home</a></li>
-                            <li <?php if ($_SERVER['REQUEST_URI'] == '/activity.php') echo 'class="active"'; ?>><a href="../activity.php">Activity</a></li>
-                            <li><a href="../logout.php">Logout</a></li>
-                        </ul>
-                    </nav>
+<div class="navbar navbar-default navbar-fixed-top">
+            
+            <div class="container">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand">DailyHabit</a>
                 </div>
+                
+                <div class="collapse navbar-collapse"></div>
+                <ul class="nav navbar-nav">
+                   <li <?php if ($_SERVER['REQUEST_URI'] == '/') echo 'class="active"'; ?>><a href="../">Home</a></li>
+                            <li <?php if ($_SERVER['REQUEST_URI'] == '/activity.php') echo 'class="active"'; ?>><a href="../activity.php">Profile</a></li>
+                            <li><a href="../activity.php">Goals</a></li>
+                            <li ><a href="../activity.php">Community</a></li>
+                            
+                </ul>
+                
+                <form  class="navbar-form navbar-right">
+                     <input type="hidden" name="task" value="logout">
+                     <button type="submit" class="btn btn-success" ><a href="../logout.php">Log Out</a></button>
+                </form>
+            </div>
+        </div>
