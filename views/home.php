69a6e94e302a57f1343450706729bb09a38c64c4
souki812 <alamisouki@gmail.com>
2016-04-12 18:23:04 -0400

    First Draft of the User Home Page

--- views/home.php
+++ views/home.php
@@ -1,8 +1,23 @@
             <div class="row">
 <?php require('menu.php'); ?>
-                <div class="col-xs-9">
-                    <article>
-                        <p>This is the home page.</p>
-                    </article>
-                </div>
-            </div>
+
+<style>
+    .box{
+        background-color: #d3d3d3
+    }
+</style>
+            <div class="container">
+    <table class="table table-striped ">
+        <tr class="success"><th>Your Daily Summary</th><th>Day Streak</th></tr>
+        <tr><td>Have you achieved today's Habit?</td><td><button class="btn btn-success marginTop">Achieved!</button>    <button class="btn btn-success marginTop">Edit Goal</button></td></tr>
+        <tr><td><input type="text" class="form-control" value="Leave a comment.."/></td></tr>
+        <tr><td><button class="btn btn-success marginTop">Post</button></td></tr>
+    </table>
+    
+    <form>
+        <div class="form-group">
+            <label>NewsFeed</label>
+        <input type="text" class="form-control" value="What's on your mind?"/>
+        </div>
+    </form>
+</div>
