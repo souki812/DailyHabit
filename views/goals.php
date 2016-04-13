a293f63daa889148d64dc44f1aeb0bbfc7e435ad
haleytaylor17 <haleytaylor17@gmail.com>
2016-04-13 16:59:37 -0400

    created goals page

---
+++ views/goals.php
@@ -0,0 +1,65 @@
+ 
+<?php require('menu.php'); ?>
+
+<style>
+    .acheived{
+    background-color:#2E2D88;
+    color:white;
+}
+
+  .future{
+    background-color:#2E2D88;
+    color:white;
+}
+
+    
+</style>
+
+
+<div class="container" id="firstContainer">
+ 
+
+<div class="page-header">
+<h1>Daily Habit App</h1>
+</div><br><br>
+ 
+ 
+
+<div class="container PageContainer" id="firstContainer">
+  
+
+        <div class="col-md-6 margintop1 acheived">
+            <h3 class="text-left"><span class="glyphicon glyphicon-ok"></span> Your Acheived Goals</h3>
+
+        </div>
+        
+
+
+
+        <div class="col-md-6 margintop1 future">
+            <h3 class="text-right"><span class="glyphicon glyphicon-road"></span> Your Future Goals</h3>
+        </div> 
+</div><br><br><br>
+
+
+
+
+<div class="jumbotron">
+ <div class="container PageContainer" id="secondcontainer">
+    
+    <div class="row margintop" class="centertext">
+        
+        <div class="col-md-12 margintop1">
+            <h3><span class="glyphicon glyphicon-pushpin"></span> Your Current Goal</h3>
+        </div>
+
+
+        <div class="text-right"><br><br>
+            <button type="submit" class = "btn btn-info btn-lg" >Edit Goal</button>
+            <button type="submit" class = "btn btn-info btn-lg" >Add New Goal</button>
+        </div> 
+
+
+
+    </div>
+    </div><br>
