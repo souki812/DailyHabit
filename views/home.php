58c1dc12adaa0fe6cffec741a88c8b888846e069
souki812 <alamisouki@gmail.com>
2016-04-13 08:41:35 -0400

    Made some changes to the Home HTML

--- views/home.php
+++ views/home.php
@@ -2,17 +2,53 @@
 <?php require('menu.php'); ?>
 
 <style>
-    .box{
-        background-color: #d3d3d3
+    
+    
+    
+    .home{
+        border: 1px solid grey;
+        
+       
+    }
+    
+    .hometop{
+     
+    }
+    
+    .comment{
+        
+        margin-top: 20px;
+    }
+    .firstbutton{
+        margin-left: 40px;
+    }
+    .secondbutton{
+        margin-left: 20px;
     }
 </style>
-            <div class="container">
-    <table class="table table-striped ">
-        <tr class="success"><th>Your Daily Summary</th><th>Day Streak</th></tr>
-        <tr><td>Have you achieved today's Habit?</td><td><button class="btn btn-success marginTop">Achieved!</button>    <button class="btn btn-success marginTop">Edit Goal</button></td></tr>
-        <tr><td><input type="text" class="form-control" value="Leave a comment.."/></td></tr>
-        <tr><td><button class="btn btn-success marginTop">Post</button></td></tr>
-    </table>
+    
+             <div class="container">
+    <div class="row">
+        
+        <div class="col-md-6 col-md-offset-3  home" >
+            <div>
+                 <h4>Your Daily Summary</h4>
+            </div>
+           
+            
+            <form method="post">
+                <div class="form-group">
+                    <label >Have you achieved today's Habit?</label>
+                    <button  class="btn btn-success firstbutton" ">Achieved!</button><button  class="btn btn-success secondbutton" >Edit Goal</button>
+                    
+                    <input type="text" class="form-control comment" value="Leave a comment.."/>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+    
+    
     
     <form>
         <div class="form-group">
