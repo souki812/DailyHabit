e9799180b0ccaf72c0444ff2f45278886b93bf06
haleytaylor17 <hmtayl13@stlawu.edu>
2016-04-18 11:56:11 -0400

    added new information to community page

---
+++ views/community.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DailyHabit</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+              
+            </div>
+
+
+
+
+            <style>
+                #leftcol {
+                    background-color:#E6E1DF;
+                    color:#cfcac8;
+                }
+                
+                 #rightcol {
+                    background-color:#E6E1DF;
+                    color:#cfcac8;
+                }
+                
+                #first {
+                    
+                    background: url("../views/images/scene1.jpg");
+                    width: 100%;
+                    background-size: cover;
+                    height: 150px;
+                    border-radius: 10px;
+                    font-family: Helveltica, serif;
+                    text-align: center;
+                    color:white;
+                }
+                
+                #second {
+                    background: url("../views/images/scene3.jpg");
+                    width: 100%;
+                    background-size: cover;
+                    height: 150px;
+                    border-radius: 10px;
+                    font-family: Helveltica, serif;
+                    text-align: center;
+                    color:white;
+                }
+                
+                #third {
+                    background: url("../views/images/scene2.jpg");
+                    width: 100%;
+                    background-size: cover;
+                    height: 100px;
+                    border-radius: 10px;
+                    font-family: Helveltica, serif;
+                    text-align: center;
+                    color:white;
+                }
+                
+                #fourth {
+                    background: url("../views/images/scene4.jpg");
+                    width: 100%;
+                    background-size: cover;
+                    height: 150px;
+                    border-radius: 10px;
+                    font-family: Helveltica, serif;
+                    text-align: center;
+                    color:white;
+                }
+                
+                
+            </style>
+            
+            
+            <!------------------------------------------------------------>
+            <h2><span class="glyphicon glyphicon-user"></span> Community</h2><br>
+            <div class="row">
+                <!--Left main column-->
+                <div class="col-md-8">
+                    <!--Left column-->
+                    <div class="col-md-6" id="leftcol" style="border:solid">
+                    <div class="media">
+                        <a href="#" class = "thumbnail pull-left">        <!-- class= pull-left -->
+                            <img src="http://d34yn14tavczy0.cloudfront.net/images/no_photo.png" width="70" height="70" alt="The Acropololis"/>
+                            <!-- <img src="views/images/goals.jpg" alt="The Acropololis"/> --> 
+                        </a>
+                        <div class="media-body"><br>
+                            <h4><a>Name: </a></h4>
+                        </div>
+                        
+                        
+                    </div>  
+                    </div>
+                    
+                    
+                    <!--Right column-->
+                    <div class="col-md-6" id="rightcol" style="border:solid">
+                    <div class="media">
+                        <a href="#" class = "thumbnail pull-left">        <!-- class= pull-left -->
+                            <img src="http://d34yn14tavczy0.cloudfront.net/images/no_photo.png" width="70" height="70" alt="The Acropololis"/> 
+                            <!-- <img src="views/images/goals.jpg" alt="The Acropololis"/> --> 
+                        </a>
+                        <div class="media-body"><br>
+                            <h4><a>Name: </a></h4>
+                        </div>
+                    </div>
+                    
+                </div>  <!--End of main column-->
+            </div>
+                
+                
+                
+                <div class="row">
+                    <!--Right main column-->
+                    <div class="col-md-4">
+                        <p> Right main content</p>
+                        
+                        
+                        <div class="centertext" id="first">
+                        <h4><br>"First we form habits, then they form us.  Conquer your bad habits, or they'll eventually conquer you."</h4>
+                        <h4 class="text-right">"-Rob Gilbert"</h4><br>
+                        </div>
+                        
+                        <div class="centertext" id="second">
+                        <h4><br>"Habits are important.  Up do 90% of our everyday behavior is based on habit.  Nearly all of what we do each day, every day, is simply habit"</h4>
+                        <h4 class="text-right">"-Jack D. Hodge"</h4><br>
+                        </div>
+                        
+                        <div class="centertext" id="third">
+                        <h4><br>"Motivation is what gets you started.  Habit is what keeps you going."</h4>
+                        <h4 class="text-right">"-Jim Rohn"</h4><br>
+                        </div>
+                        
+                        <div class="centertext" id="fourth">
+                        <h4><br>"Focus on the good habit you want to do, not the bad habit you don't want to do.  Overcome the bad with the good."</h4>
+                        <h4 class="text-right">"-Joyce Meyer"</h4><br>
+                        </div>
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                    </div>
+                </div>
+<!------------------------------------------------------------>
+
+
+    
+       
+    
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        
+        
+        
+    
+</html>
