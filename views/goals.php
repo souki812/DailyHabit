7f180dbe2d3af57be800455c2349e1d3e063a071
souki812 <alamisouki@gmail.com>
2016-04-18 12:54:20 -0400

    Working on the goals page

--- views/goals.php
+++ views/goals.php
@@ -2,7 +2,34 @@
 <?php require('menu.php'); ?>
 
 <style>
-
+.home{
+        border: 1px solid grey;
+        
+       
+    }
+    
+    .hometop{
+     
+    }
+    
+      .comment{
+        border: 1px solid grey;
+        margin-bottom: 20px;
+    }
+    .firstbutton{
+        margin-top: 20px;
+        margin-left: 150px;
+        margin-bottom: 10px;
+    }
+    .secondbutton{
+        margin-left: 20px;
+        margin-bottom: 10px;
+    }
+    
+    .page{
+       
+         padding-top: 100px; 
+    }
 </style>
 <div class="container page">
     <div class="row">
@@ -15,19 +42,50 @@
             
             <form method="post">
                 <div class="form-group">
-                    <label >Have you achieved today's Habit?</label>
-                    <button  class="btn btn-success firstbutton" ">Achieved!</button><button  class="btn btn-success secondbutton" >Edit Goal</button>
-                    
-                    <input type="text" class="form-control comment" value="Leave a comment.."/>
-                    
-                    
+                    <button  class="btn btn-success secondbutton" >Edit List</button>
                 </div>
             </form>
         </div>
-    </div><br>
- 
-
-
-
     </div>
-    </div><br>
+ 
+</div>
+
+<div class="container page">
+    <div class="row">
+        
+        <div class="col-md-6 col-md-offset-3  home" >
+           
+           
+            
+            <form method="post">
+                <div class="form-group">
+                   
+                    <label >Future Goals</label>
+                    <button  class="btn btn-success firstbutton" ">Edit List</button>
+                   
+                </div>
+                
+            </form>
+        </div>
+    </div>
+ 
+</div>
+<div class="container page">
+    <div class="row">
+        
+        <div class="col-md-6 col-md-offset-3  home" >
+            <div>
+                 <h4>Current Goal</h4>
+            </div>
+           
+            
+            <form method="post">
+                <div class="form-group">
+                    <button  class="btn btn-success secondbutton" >Edit Goal</button>
+                </div>
+            </form>
+        </div>
+    </div>
+ 
+</div>
+  
--- views/menu.php
+++ views/menu.php
@@ -15,7 +15,7 @@
                    <li <?php if ($_SERVER['REQUEST_URI'] == '/home.php') echo 'class="active"'; ?>><a href="../home.php">Home</a></li>
                             <li <?php if ($_SERVER['REQUEST_URI'] == '/activity.php') echo 'class="active"'; ?>><a href="../activity.php">Profile</a></li>
                             <li <?php if ($_SERVER['REQUEST_URI'] == '/goals.php') echo 'class="active"'; ?>><a href="../goals.php">Goals</a></li>
-                            <li ><a href="../activity.php">Community</a></li>
+                            <li <?php if ($_SERVER['REQUEST_URI'] == '/community.php') echo 'class="active"'; ?>><a href="../community.php">Community</a></li>
                             
                 </ul>
                 
