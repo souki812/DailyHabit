39deb9b3ae644f094feda01c91b382ba880b7399
Souki <alamisouki@gmail.com>
2016-04-27 19:34:46 -0400

    Validate function

--- authenticate.php
+++ authenticate.php
@@ -20,63 +20,16 @@ if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['task'])
         // Attempt registration
         if ($_POST['task'] == 'register') {
          
-                  if(!$_POST['email']){
-                     $error.="<br />Please enter your email";
-                     
-                  }else if( !filter_var($_POST['emai'], FILTER_VALIDATE_EMAIL)){
-                     $error.="<br />Please enter a valid email address";
-                  }
-                  
-                   if(!$_POST['password']){
-                     $error.="<br />Please enter a password";
-                   }else{
-                     if (strlen($_POST['password'])<6) $error.="<br />Please enter a password with at least 6 characters";
-                     
-                   }
-                   
-                    if(!$_POST['first']){
-                     $error.="<br />Please enter your first name";
-                     
-                  }
-                  
-                   if(!$_POST['last']){
-                     $error.="<br />Please enter your last name";
-                     
-                  }
-                  
-                   if(!$_POST['email']){
-                     $error.="<br />Please enter your email";
-                     
-                  }
-                  
-                   if(!$_POST['gender']){
-                     $error.="<br />Please enter your gender";
-                     
-                  }
-                  
-                   if(!$_POST['age']){
-                     $error.="<br />Please enter your age";
-                     
-                  }
-         
-            if($error){
-               echo "There were errors in your signup!".$error;
-            }else{
                 $success = $user->register( $_POST['first'], $_POST['last'], $_POST['email'], $_POST['password'],  $_POST['gender'], $_POST['age']);
+              
             
             if ($success) {
                 $_SESSION['message'] = 'Registered! You can now log in.';
             } else {
                 $_SESSION['message'] = 'Sorry, there is an account with the same email.';
             }
-            }
-         
-         
-       
-        }
-        
-        // Or attempt login
-        elseif ($_POST['task'] == 'login') {
+             }
+          elseif ($_POST['task'] == 'login') {
             $user_id = $user->login($_POST['email'], $_POST['password']);
             
             if (isset($user_id)) {
@@ -85,9 +38,15 @@ if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['task'])
             } else {
                 $_SESSION['message'] = 'Wrong username or password.';
             }
+         
+       
+        }
+        
+        // Or attempt login
+       
         }
     }
-}
+
 
 // Return home
 header('Location: ./home.php');
