9fc78a2377917e42cf2c6339c9682fad65949264
haleytaylor17 <hmtayl13@stlawu.edu>
2016-04-27 12:33:47 -0400

    working on profile page

--- views/activity.php
+++ views/activity.php
@@ -36,9 +36,79 @@
         margin-right: 20px;
         margin-bottom: 20px;
     }
+    
+    .circleBase {
+        border-radius: 50%;
+        behavior: url(PIE.htc); /* remove if you don't care about IE8 */
+        }
+
+    .type1 {
+        /*background: url("views/images/manon.jpg");*/
+        margin-left: 310px;
+        margin-top: -80px;
+        border-radius: 50%/50%; 
+        width: 150px;
+        height: 150px;
+        border: 3px solid #000;
+    }
+    
+    .words {
+        margin:auto;
+        margin-left: 350px;
+        margin-top: 30px;
+    }
+    
+    /*
+    .talkbubble {
+        width: 120px;
+        height: 80px;
+        background: gray;
+        position: relative;
+        -moz-border-radius: 10px;
+        -webkit-border-radius: 10px;
+        border-radius: 10px;
+        border: 1px solid grey;
+        margin-bottom: 20px;
+    }
+        
+    .talkbubble:before {
+        content:"";
+        position: absolute;
+        right: 100%;
+        top: 26px;
+        width: 0;
+        height: 0;
+        border-top: 13px solid transparent;
+        border-right: 26px solid red;
+        border-bottom: 13px solid transparent;
+        border: 1px solid grey;
+        margin-bottom: 20px;
+    }
+    */
+    
 </style>
  
-
+ <!--New layout-->
+ <br><br><br><br><br><br><br><br><div class="row">
+    <div class="col-md-8 col-md-offset-2  profile">
+        <div><img class="circleBase type1" src="http://d34yn14tavczy0.cloudfront.net/images/no_photo.png"></div>
+        <?php foreach ($selection as $row): ?>
+            <h3 class="words"><?php echo htmlentities($row['first'], ENT_QUOTES, 'utf-8'); ?></h3> 
+            <h3 class="words"><?php echo htmlentities($row['last'], ENT_QUOTES, 'utf-8'); ?></h3>
+            <h4 class="words"><?php echo htmlentities($row['gender'], ENT_QUOTES, 'utf-8'); ?></h4>
+            <h4 class="words"><?php echo htmlentities($row['age'], ENT_QUOTES, 'utf-8'); ?></h4>
+            <label class="words">About me:</label>
+            <h4 class="words"><?php echo htmlentities($row['biography'], ENT_QUOTES, 'utf-8'); ?></h4>
+        <?php endforeach; ?><br>
+        <button  class="btn btn-success firstbutton"  data-toggle="modal" data-target="#modal1" >Add Biography</button>
+        <button  class="btn btn-success secondbutton" data-toggle="modal" data-target="#modal2" >Post a comment</button>
+    </div>
+ </div>
+ 
+ 
+ 
+ 
+<!--Old layout-->
 <div class="container page" id="firstContainer">
  <div class="row">
         
