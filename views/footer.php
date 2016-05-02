ebdabe58f502768ce1b7f8f1e737227079cfa595
haleytaylor17 <hmtayl13@stlawu.edu>
2016-05-02 16:55:40 -0400

    the reset button now works for the progress bar

--- views/footer.php
+++ views/footer.php
@@ -49,25 +49,9 @@
                   
                   // If the reset button is pressed
                   $("#reset").click(function() {
-      
-                        $.ajax({
-                              type: 'POST',
-                              url: 'getprogress.php',
-                              data: { val:val },
-                              success: function(response) {
-            
-                                    val = response;
-                                    
-                                    if (val <=100) {
-                                          val = 0;
-                                    }
-                                    //$('.progress-bar').css('width', val+'%').attr('aria-valuenow', val);
-                                    //$("#progress1").html(response + '%');
-                              }
-                        });  
+                        $('.progress-bar').css('width', '0%').attr('aria-valuenow', 0);
+                        $("#progress1").html('0%');       
                   });
-                  
-
       });
       </script>
      
